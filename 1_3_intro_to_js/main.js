efef7dc5022e15e47d400c38144bc7034f1e707c
lolusername <to.atilio@gmail.com>
2022-09-20 19:32:33 -0400

    bonus

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -2,4 +2,15 @@ let counter = 0;
 const display = document.querySelector("#display");
 const btn = document.querySelector("#increment");
 
-btn.addEventListener("click", () => (display.innerHTML = ++counter));
+const bonusObj = {};
+
+btn.addEventListener("click", () => {
+  display.innerHTML = ++counter;
+
+  //   bonus
+  const dateString = String(Date.now());
+
+  bonusObj[dateString] = counter;
+
+  console.log(bonusObj);
+});
