c4ba58a79f38584c153c76c04a6a3faeed70638f
lolusername <to.atilio@gmail.com>
2022-09-20 19:11:41 -0400

    hw1

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,5 @@
-console.log('hello world');
+let counter = 0;
+const display = document.querySelector("#display");
+const btn = document.querySelector("#increment");
+
+btn.addEventListener("click", () => (display.innerHTML = ++counter));
